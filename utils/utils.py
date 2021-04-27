66801c1fb8d6d058bf7b0506f75f46243384288d
Ragav Sachdeva <ragdev707@gmail.com>
2021-04-27 22:30:45 +0930

    update eval

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,3 @@
-"""
-Authors: Wouter Van Gansbeke, Simon Vandenhende
-Licensed under the CC BY-NC 4.0 license (https://creativecommons.org/licenses/by-nc/4.0/)
-"""
 import os
 import torch
 import numpy as np
